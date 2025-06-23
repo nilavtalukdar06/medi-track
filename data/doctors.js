5260e920f5854f72d0a6f8e1d7f36284162fef67
Nilav <nilavtalukdar06@gmail.com>
2025-06-23 20:11:37 +0530

    added the data of 10 doctors

---
+++ data/doctors.js
@@ -0,0 +1,17 @@
+export const doctors = [
+  {
+    name: "Dr. Ayesha Khan",
+  },
+  {
+    name: "Dr. Rohan Mehta",
+  },
+  {
+    name: "Dr. Priya Nair",
+  },
+  {
+    name: "Dr. Arvind Patel",
+  },
+  {
+    name: "Dr. Sneha Roy",
+  },
+];
