0b2cc18347f5e83428f699043c56c015a9f54c13
Nilav <nilavtalukdar06@gmail.com>
2025-06-23 20:54:32 +0530

    completed the ui of appointment form

--- components/appointment-form.jsx
+++ components/appointment-form.jsx
@@ -10,13 +10,29 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { CalendarIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Calendar } from "./ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
 
 export default function AppointmentForm() {
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState(null);
+
   return (
     <form className="flex flex-col gap-y-4">
       <div className="space-y-1.5">
-        <Label>Doctor</Label>
-        <Select className="w-full">
+        <Label className="text-xs text-gray-500" htmlFor="doctor">
+          Doctor
+        </Label>
+        <Select className="w-full" id="doctor">
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Select a doctor" />
           </SelectTrigger>
@@ -32,6 +48,52 @@ export default function AppointmentForm() {
           </SelectContent>
         </Select>
       </div>
+      <div className="space-y-1.5">
+        <Label className="text-xs text-gray-500" htmlFor="reason">
+          Reason for appointment
+        </Label>
+        <Input placeholder="ex: Annual monthly check-up" id="reason" />
+      </div>
+      <div className="space-y-1.5">
+        <Label className="text-xs text-gray-500" htmlFor="comments">
+          Additional comments/notes
+        </Label>
+        <Textarea
+          placeholder="ex: Prefer afternoon appointments, if possible"
+          id="comments"
+        />
+      </div>
+      <div className="space-y-1.5">
+        <Label className="text-xs text-gray-500" htmlFor="date">
+          Expected appointment date
+        </Label>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date"
+              className="w-full justify-start text-gray-600 font-normal"
+            >
+              <CalendarIcon />
+              {date ? date.toLocaleDateString() : "Select date"}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={date}
+              captionLayout="dropdown"
+              onSelect={(date) => {
+                setDate(date);
+                setOpen(false);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+      <Button className="bg-[#24AE7C] hover:bg-green-700 text-white">
+        Request Appointment
+      </Button>
     </form>
   );
 }
