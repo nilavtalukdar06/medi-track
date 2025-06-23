f317053cfd43ced42b1720f9fea7f62c0bc2d11e
Nilav <nilavtalukdar06@gmail.com>
2025-06-23 20:33:25 +0530

    updated doctors data

--- components/appointment-form.jsx
+++ components/appointment-form.jsx
@@ -1,7 +1,37 @@
+"use client";
+import { doctors } from "@/data/doctors";
+import { Label } from "./ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 export default function AppointmentForm() {
   return (
-    <form>
-      <p>Appointment form</p>
+    <form className="flex flex-col gap-y-4">
+      <div className="space-y-1.5">
+        <Label>Doctor</Label>
+        <Select className="w-full">
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Select a doctor" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Doctors</SelectLabel>
+              {doctors.map((doctor) => (
+                <SelectItem value={doctor.name} key={doctor.id}>
+                  {doctor.name}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
     </form>
   );
 }
--- data/doctors.js
+++ data/doctors.js
@@ -1,17 +1,22 @@
 export const doctors = [
   {
+    id: 1,
     name: "Dr. Ayesha Khan",
   },
   {
+    id: 2,
     name: "Dr. Rohan Mehta",
   },
   {
+    id: 3,
     name: "Dr. Priya Nair",
   },
   {
+    id: 4,
     name: "Dr. Arvind Patel",
   },
   {
+    id: 5,
     name: "Dr. Sneha Roy",
   },
 ];
