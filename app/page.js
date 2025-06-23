f9dc17897f8a9cead74f71dbaf0e80503109efc2
Nilav <nilavtalukdar06@gmail.com>
2025-06-23 15:39:56 +0530

    added shadcn ui

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <section>
+    <section className="p-4">
       <h1>Medi Track</h1>
+      <Button variant="destructive">Click Me 😊</Button>
     </section>
   );
 }
