be22104415c75e7dd4c908f931cd54a9215667c1
Nilav <nilavtalukdar06@gmail.com>
2025-06-23 16:23:27 +0530

    completed appointment form layout

--- components/appointment-form-layout.jsx
+++ components/appointment-form-layout.jsx
@@ -1,19 +1,32 @@
 "use client";
 import Image from "next/image";
 import { motion } from "motion/react";
+import { TextAnimate } from "./magicui/text-animate";
 
 export default function AppointmentFormLayout() {
   return (
-    <article className="max-w-md p-4 my-8 mx-auto">
+    <article className="max-w-md p-4 my-4 mx-auto">
       <motion.div
-        className="w-full flex justify-center items-center gap-x-4"
+        className="w-full flex justify-center items-center"
         initial={{ opacity: 0 }}
-        animate={{ opacity: 100 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 2 }}
       >
         <Image src="/logo.svg" height={36} width={36} alt="logo image" />
-        <h2 className="text-2xl font-bold text-gray-600">Medi Track</h2>
       </motion.div>
+      <div className="my-4 flex flex-col justify-center items-center gap-y-4">
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="text-xl font-semibold text-gray-600"
+        >
+          Hey there 👋
+        </motion.h2>
+        <TextAnimate className="text-sm text-gray-400 tracking-wide leading-snug">
+          Request a new appointment in 10 seconds
+        </TextAnimate>
+      </div>
     </article>
   );
 }
