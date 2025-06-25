241d7255bce8c4913feb002025e954f5d2c1edc8
Nilav <nilavtalukdar06@gmail.com>
2025-06-25 20:35:30 +0530

    added appointment creation api

---
+++ app/api/create-appointment/route.js
@@ -0,0 +1,49 @@
+import connection from "@/db/mongodb";
+import appointmentModel from "@/model/appointment.model";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json(
+        {
+          message: "unauthorized",
+        },
+        { status: 401 }
+      );
+    } else {
+      const data = await request.json();
+      if (
+        !data.doctor ||
+        !data.reason_for_appointment ||
+        !data.additional_comments ||
+        !data.expected_date
+      ) {
+        return NextResponse.json(
+          {
+            message: "incomplete fields",
+          },
+          { status: 400 }
+        );
+      }
+      await connection();
+      await appointmentModel.create({ ...data, created_by: userId });
+      return NextResponse.json(
+        {
+          message: "appointment created successfully",
+        },
+        { status: 201 }
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: `internal server error, error: ${error.message}`,
+      },
+      { status: 500 }
+    );
+  }
+}
