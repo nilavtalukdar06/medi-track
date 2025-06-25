3b9c76c54d2782a84d66c298c65763e0892735ab
Nilav <nilavtalukdar06@gmail.com>
2025-06-25 22:31:32 +0530

    get api endpoint for all doctors data

---
+++ app/api/get-appointments/route.js
@@ -0,0 +1,36 @@
+import connection from "@/db/mongodb";
+import appointmentModel from "@/model/appointment.model";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+    if (!userId || user?.privateMetadata?.role !== "admin") {
+      return NextResponse.json(
+        {
+          message: "unauthorized",
+        },
+        { status: 401 }
+      );
+    } else {
+      await connection();
+      const result = await appointmentModel.find({});
+      return NextResponse.json(
+        {
+          message: result,
+        },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: `internal server error, error: ${error.message}`,
+      },
+      { status: 500 }
+    );
+  }
+}
