207ed393afdc679ac8f53c09605e162beb1dc384
Nilav <nilavtalukdar06@gmail.com>
2025-06-25 20:19:51 +0530

    Add appointment model schema with required fields for doctor, reason, comments, date, and creator

---
+++ model/appointment.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema({
+  doctor: {
+    type: String,
+    required: [true, "doctor's name is required"],
+  },
+  reason_for_appointment: {
+    type: String,
+    required: [true, "reason is required"],
+  },
+  additional_comments: {
+    type: String,
+    required: [true, "additional comments are required"],
+  },
+  expected_date: {
+    type: String,
+    required: [true, "date is required"],
+  },
+  created_by: {
+    type: String,
+    required: true,
+  },
+});
+
+const appointmentModel =
+  mongoose.models.appointmentModel ||
+  mongoose.model("appointmentModel", appointmentSchema);
+
+export default appointmentModel;
