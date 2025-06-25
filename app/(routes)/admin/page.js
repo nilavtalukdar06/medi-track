3d3fced3feae220da4b70c3b2208ee4d1cc716ba
Nilav <nilavtalukdar06@gmail.com>
2025-06-25 21:25:00 +0530

    added heading to the admin page

--- app/(routes)/admin/page.js
+++ app/(routes)/admin/page.js
@@ -1,6 +1,7 @@
 import Navbar from "@/app/navbar";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { TextAnimate } from "@/components/magicui/text-animate";
 
 export default async function AdminPanel() {
   const user = await currentUser();
@@ -9,10 +10,15 @@ export default async function AdminPanel() {
   }
 
   return (
-    <section>
+    <section className="w-full">
       <Navbar />
-      <div className="p-4">
-        <h1>Admin Panel</h1>
+      <div className="px-6 py-2">
+        <TextAnimate className="text-3xl text-gray-600 font-semibold tracking-wide text-center sm:text-start">
+          Welcome Admin
+        </TextAnimate>
+        <TextAnimate className="text-sm text-gray-500 tracking-wide font-light my-1.5 text-center sm:text-start">
+          Start day with managing new appointments
+        </TextAnimate>
       </div>
     </section>
   );
