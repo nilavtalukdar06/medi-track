0876a1cfee74fa8e1ef874b7d756e73336683bc0
Nilav <nilavtalukdar06@gmail.com>
2025-06-25 22:39:17 +0530

    Add loading state and fetchAppointments function to AppointmentStatistics component

--- components/appointment-statistics.jsx
+++ components/appointment-statistics.jsx
@@ -1,8 +1,36 @@
 "use client";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const StatisticsContext = createContext();
 
 export default function AppointmentStatistics({ children }) {
-  return <StatisticsContext.Provider>{children}</StatisticsContext.Provider>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const fetchAppointments = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch("/api/get-appointments");
+      if (!response.ok) {
+        throw new Error(`error: ${response.status}, ${response.statusText}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      if (data.length === 0) {
+        setData([]);
+      } else {
+        setData([...data]);
+      }
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <StatisticsContext.Provider value={{ isLoading, data, fetchAppointments }}>
+      {children}
+    </StatisticsContext.Provider>
+  );
 }
