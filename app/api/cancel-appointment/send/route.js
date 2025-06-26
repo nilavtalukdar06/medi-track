95fe67a1c4e3f7e7bf5dab2548d0ee6f73fb0b2c
Nilav <nilavtalukdar06@gmail.com>
2025-06-26 15:33:05 +0530

    made the api route for sending cancellation emails to the patients

--- app/api/cancel-appointment/send/route.js
+++ app/api/cancel-appointment/send/route.js
@@ -1,23 +1,33 @@
 import { CancelAppointment } from "@/components/email/cancel-appointment.jsx";
+import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(request) {
   try {
+    const { reason, comments, email } = await request.json();
+    if (!reason || !comments || !email) {
+      return NextResponse.json(
+        {
+          message: "incomplete fields",
+        },
+        { status: 400 }
+      );
+    }
     const { data, error } = await resend.emails.send({
-      from: "Acme <nilavtalukdar06@imagify.space>",
-      to: ["nilavtalukdar9@gmail.com"],
+      from: "MediTrack <nilavtalukdar06@imagify.space>",
+      to: [email],
       subject: "Appointment Cancellation",
-      react: CancelAppointment({ reason: "test", comments: "test" }),
+      react: CancelAppointment({ reason: reason, comments: comments }),
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return NextResponse.json({ error }, { status: 500 });
     }
 
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
