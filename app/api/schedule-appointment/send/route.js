2ee5984ed011acda827a18fa7fa609c1af930793
Nilav <nilavtalukdar06@gmail.com>
2025-06-26 22:26:25 +0530

    Implement appointment scheduling API with email notification and admin authorization

---
+++ app/api/schedule-appointment/send/route.js
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+import ScheduleAppointment from "@/components/email/schedule-appintment";
+import { Resend } from "resend";
+import connection from "@/db/mongodb";
+import appointmentModel from "@/model/appointment.model";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request) {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+    if (!userId || user?.privateMetadata?.role !== "admin") {
+      return NextResponse.json(
+        {
+          message: "unauthorized",
+        },
+        { status: 401 }
+      );
+    }
+    const { reason, comments, date, time, email, appointment_id } =
+      await request.json();
+    if (!reason || !comments || !date || !time || !email || !appointment_id) {
+      return NextResponse.json(
+        {
+          message: "incomplete fields",
+        },
+        { status: 400 }
+      );
+    }
+    await connection();
+    const appointment = await appointmentModel.findOne({ _id: appointment_id });
+    if (!appointment) {
+      return NextResponse.json(
+        {
+          message: "no appointments found",
+        },
+        { status: 403 }
+      );
+    }
+    const { data, error } = await resend.emails.send({
+      from: "MediTrack <nilavtalukdar06@imagify.space>",
+      to: [email],
+      subject: "Appointment Scheduling",
+      react: ScheduleAppointment({
+        reason: reason,
+        comments: comments,
+        date: date,
+        time: time,
+      }),
+    });
+
+    if (error) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+
+    await appointmentModel.updateOne(
+      { _id: appointment_id },
+      { $set: { status: "accepted" } }
+    );
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
