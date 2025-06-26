8848b86de56ffe6c085d543a9f75a2ecadc996de
Nilav <nilavtalukdar06@gmail.com>
2025-06-26 06:57:25 +0530

    added animation to the cards

--- components/cards.jsx
+++ components/cards.jsx
@@ -1,5 +1,7 @@
+"use client";
 import { Calendar, Clock, TriangleAlert } from "lucide-react";
 import { Skeleton } from "./ui/skeleton";
+import { motion } from "motion/react";
 
 export default function Cards() {
   return (
@@ -7,7 +9,11 @@ export default function Cards() {
       {false ? (
         <Skeleton className="w-full h-[145.587px]" />
       ) : (
-        <div className="w-full p-6 border shadow-sm rounded-xl [background:linear-gradient(117.58deg,rgba(215,237,237,0.16)_-47.79%,rgba(204,235,235,0)_100%)]">
+        <motion.div
+          initial={{ scale: 0.8 }}
+          animate={{ scale: 1 }}
+          className="w-full p-6 border shadow-sm rounded-xl [background:linear-gradient(117.58deg,rgba(215,237,237,0.16)_-47.79%,rgba(204,235,235,0)_100%)]"
+        >
           <div className="flex gap-x-3 justify-start items-center">
             <Calendar color="#FFD147" />
             <p className="text-2xl font-semibold text-[#FFD147]">94</p>
@@ -15,12 +21,16 @@ export default function Cards() {
           <p className="mt-5 mb-1 text-sm md:text-base text-gray-600">
             Total number of scheduled appointments
           </p>
-        </div>
+        </motion.div>
       )}
       {false ? (
         <Skeleton className="w-full h-[145.587px]" />
       ) : (
-        <div className="w-full p-6 border shadow-sm rounded-xl [background:linear-gradient(117.58deg,rgba(215,237,237,0.16)_-47.79%,rgba(204,235,235,0)_100%)]">
+        <motion.div
+          initial={{ scale: 0.8 }}
+          animate={{ scale: 1 }}
+          className="w-full p-6 border shadow-sm rounded-xl [background:linear-gradient(117.58deg,rgba(215,237,237,0.16)_-47.79%,rgba(204,235,235,0)_100%)]"
+        >
           <div className="flex gap-x-3 justify-start items-center">
             <Clock color="#79B5EC" />
             <p className="text-2xl font-semibold text-[#79B5EC]">32</p>
@@ -28,12 +38,16 @@ export default function Cards() {
           <p className="mt-5 mb-1 text-sm md:text-base text-gray-600">
             Total number of pending appointments
           </p>
-        </div>
+        </motion.div>
       )}
       {false ? (
         <Skeleton className="w-full h-[145.587px]" />
       ) : (
-        <div className="w-full p-6 border shadow-sm rounded-xl [background:linear-gradient(117.58deg,rgba(215,237,237,0.16)_-47.79%,rgba(204,235,235,0)_100%)]">
+        <motion.div
+          initial={{ scale: 0.8 }}
+          animate={{ scale: 1 }}
+          className="w-full p-6 border shadow-sm rounded-xl [background:linear-gradient(117.58deg,rgba(215,237,237,0.16)_-47.79%,rgba(204,235,235,0)_100%)]"
+        >
           <div className="flex gap-x-3 justify-start items-center">
             <TriangleAlert color="#FF4F4E" />
             <p className="text-2xl font-semibold text-[#FF4F4E]">56</p>
@@ -41,7 +55,7 @@ export default function Cards() {
           <p className="mt-5 mb-1 text-sm md:text-base text-gray-600">
             Total number of cancelled appointments
           </p>
-        </div>
+        </motion.div>
       )}
     </section>
   );
