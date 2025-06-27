17634aa518481ae33d7d22c37761d334d0f34890
Nilav <nilavtalukdar06@gmail.com>
2025-06-27 10:54:32 +0530

    updated middleware

--- middleware.js
+++ middleware.js
@@ -1,28 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
-
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR"],
-    }),
-  ],
-});
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/", "/api(.*)"]);
 
-const combinedMiddleware = createMiddleware(
-  aj,
-  clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) {
-      await auth.protect();
-    }
-  })
-);
-
-export default combinedMiddleware;
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
